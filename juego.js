d8468b0f1147c502d7b4ce582e2b7eebec3ee8fc
daianaddiaz <zirconico@gmail.com>
2026-05-01 21:57:20 -0300

    agregado de spritesheets

--- juego.js
+++ juego.js
@@ -1,28 +1,48 @@
 class Juego {
-    pixiApp;
-    npcs = [];
-    cebador;
-    width;
-    height;
 
  constructor() {
-    this.width = innerWidth
-    this.height = innerHeight
-    this.initPIXI()
+    this.pixiApp;
+    this.pixiInicializando = false;
+    this.gameObjetcs = []
+    this.teclado = {};
+    this.ahora = performance.now();
+
+    this.init();
  }
 
- async initPIXI(){
+ async init(){
     this.pixiApp = new PIXI.Application();
 
     const opcionesDePixi = {
         background: "#627480",
-        width: this.width,
-        height: this.height,
+        width: this.innerWidth,
+        height: this.innerHeight,
     };
 
  await this.pixiApp.init(opcionesDePixi);
 
  document.body.appendChild(this.pixiApp.canvas);
+    this.pixiInicializando = true;
+    await this.precargarAssets();
+
+    this.crearEventListeners();
+
+    this.crearNivel();
+ }
+
+ crearEventListeners() {
+   window.onkeydown = (evento) => {
+      this.teclado[evento.key.toLowerCase()];
+   };
+
+   window.onkeyup = (evento) => {
+      delete this.teclado[evento.key.toLowerCase()];
+   };
 
  }
+
+ async precargarAssets(){
+   
+ }
+
 }
