66e0c40c66d4e711ac0d2527bc537565bb9186bb
daianaddiaz <zirconico@gmail.com>
2026-06-01 10:05:13 -0300

    nuevos adds en codigo

--- clases/enemigos.js
+++ clases/enemigos.js
@@ -26,5 +26,71 @@ class Enemigo extends EntidadConEmocion {
         this.direccion = opciones.direccionInicial ?? "down";
         this.ultimoEstadoDeMovimiento = this.estado;
 
-    }
+        this.spritesAnimados = {
+            idle: {},
+            walk: {},           
+        };
+
+    this.listaDeSprites = [];
+    this.sprite = null;
+
+    this.estatico = false;
+
+    this.friccion = 0.9;
+    this.nodoDelCaminoActual = 0;
+    this.asignarTarget(this.juego.nivel.nodosDelCamino[0])
+
+    this.cargarSpritesAnimados(this.dataJson);
+    this.spriteSplat = this.crearSpriteSplat(juego.assetsSplat);
+    this.cambiarAnimacion(this.estado, this.direccion);
+    
+    this.crearFSMparaComportamientos();
+    
+    this.render();
+}
+
+asignarTarget(obj) {
+    this.objTarget = obj;
+    this.targetX = obj.x;
+    this.targetY = obj.y;
+  }
+
+activar() {
+    this.activo = true;
+  }
+
+desactivar() {
+    this.activo = false;
+  }
+
+resetear() {
+    this.desactivar();
+    this.nodoDelCaminoActual = 0;
+    this.asignarTarget(this.juego.nivel.nodosDelCamino[0]);
+    this.cambiarAnimacion(this.estado, this.direccion);
+  }
+
+crearSpriteAnimado(frames, nombre, opciones = {}) {
+    const spriteAnimado = new PIXI.AnimatedSprite(frames);
+
+    spriteAnimado.label = nombre;
+    spriteAnimado.visible = false;
+    spriteAnimado.loop = opciones.loop ?? true;
+    spriteAnimado.animationSpeed = opciones.animationSpeed ?? 0.12;
+    spriteAnimado.scale.set(opciones.scale ?? 1);
+    this.configurarOrigen(spriteAnimado);
+    spriteAnimado.play();
+
+    spriteAnimado.onComplete = () => {
+      if (!spriteAnimado.loop) {
+        this.cambiarAnimacion("idle", this.direccion);
+      }
+    };
+
+    this.listaDeSprites.push(spriteAnimado);
+    this.container.addChild(spriteAnimado);
+
+    return spriteAnimado;
+  }
+
 }
